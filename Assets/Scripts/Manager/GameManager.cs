ebbf67e7a03543515af47445704957812fbb97f0
Chanel Maughmer <csmaughmer@student.fullsail.edu>
2026-08-03 04:16:22 -0500

    Add GameManager with three-life system
    Created a GameManager script that initializes the player with three lives, tracks the remaining lives, removes a life when the player is hit, and triggers a Game Over state when no lives remain.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,16 +1,92 @@
 using UnityEngine;
 
+/*
+ * Script Name: GameManager
+ * Purpose: Keeps track of the player's lives and overall game state.
+ */
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Singleton
+
+    // Allows other scripts to easily access the GameManager.
+    public static GameManager Instance;
+
+    #endregion
+
+    #region Inspector Settings
+
+    [Header("Player Settings")]
+
+    // Number of lives the player starts with.
+    [SerializeField] private int startingLives = 3;
+
+    #endregion
+
+    #region Private Variables
+
+    // Current number of lives remaining.
+    private int currentLives;
+
+    #endregion
+
+    #region Public Properties
+
+    // Allows other scripts to read the current lives.
+    public int CurrentLives
     {
-        
+        get { return currentLives; }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    #region Unity Methods
+
+    private void Awake()
     {
-        
+        // Create the singleton.
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        currentLives = startingLives;
     }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Removes one life from the player.
+    /// </summary>
+    public void LoseLife()
+    {
+        currentLives--;
+
+        Debug.Log("Lives Remaining: " + currentLives);
+
+        if (currentLives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over");
+
+        // We'll add the Game Over screen later.
+    }
+
+    #endregion
 }
